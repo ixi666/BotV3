a6eb6a49185657cf668fd528cbefcc3334fb024e
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-19 07:53:22 -0600

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
   let chat = global.DATABASE._data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está prohibido*')
+  if (!chat.isBanned) return m.reply('*Este chat no esta baneado sonso*')
   chat.isBanned = false
-  m.reply('* chat desbaneado!*')
+  m.reply('* Desbaneado*')
 }
 handler.command = /^unbanchat2$/i
 handler.rowner = true
