08568f5df7e183ce2df79aeb7386ea74dd23cb00
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-19 20:32:36 -0600

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'          
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+conn.sendButtonImg(m.chat, json, '*Hentai By ixxi*', '©The ixxi - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 module.exports = handler
