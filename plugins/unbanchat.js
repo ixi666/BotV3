ea644e3d0f642bf965f59149f47a565f77cdd31e
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-19 20:43:57 -0600

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
   let chat = global.DATABASE._data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está prohibido!!*')
+  if (!chat.isBanned) return m.reply('*Este chat no está prohibido*')
   chat.isBanned = false
-  m.reply('* chat desbaneado!*')
+  m.reply('*chat desbaneado*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['General']
