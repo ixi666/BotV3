224ddbe739b8bd10f889171f150d673f76531cdd
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-26 11:32:37 -0600

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, participants }) => {
 // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
 if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
-chat.isBanned = owner
+if (chat.isBanned) return m.reply('*Este chat ya estaba baneado*')
+chat.isBanned = true
  
 m.reply('*Este chat ha sido baneado*')
 // } else m.reply('Aquí hay un número de un host...')
@@ -11,4 +11,4 @@ m.reply('*Este chat ha sido baneado*')
 handler.command = /^banchat$/i
 handler.group = true
 handler.admin = true
-module.exports = owner
+module.exports = handler
