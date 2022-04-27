e0e77002cdcd06c674a30a298516b0f09efb67b5
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-27 13:46:56 -0600

    Update menu de comandos.js

--- plugins/menu de comandos.js
+++ plugins/menu de comandos.js
@@ -119,7 +119,7 @@ _${usedPrefix}play4
  _${usedPrefix}disable *autoread*_
 ▋▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, 'ixxi - Bot', '', ``, '', ``, '', ``, m, false, { contextInfo: { mentionedJid }})  
+conn.send3ButtonImg(m.chat, pp, menu, '@Ixxi - Bot', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂', `#grupos`, m, false, { contextInfo: { mentionedJid }})  
 //await await await await await await conn.sendFile(m.chat, vn, 'mariana.mp3', null, m, true, {
 //type: 'audioMessage', 
 //ptt: true 
